14b6535f50023e09e5736d0e4d2df7a7a0f4a319
Herberth Leão <hleaomachado@gmail.com>
2020-03-29 23:04:47 -0300

    Add license header

--- server/src/utils/generateUniqueId.js
+++ server/src/utils/generateUniqueId.js
@@ -1,4 +1,13 @@
-const crypto = require('crypto');
+/**
+ * Copyright (c) 2020 Herberth Leão
+ *
+ * This file is part of Be The Hero package.
+ *
+ * Distributed under the Azllan Public Software License, Version 1.0. (See
+ * accompanying file LICENSE.md or copy at http://azllan.io/licenses/apsl).
+ */
+
+ const crypto = require('crypto');
 
 module.exports = function generateUniqueId() {
     return crypto.randomBytes(4).toString('HEX');
